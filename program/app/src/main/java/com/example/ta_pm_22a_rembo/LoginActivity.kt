6f93a30e1d08d67cb866959ab2354a6bf8ab67a8
TediSuryaBudi <dibu68430@gmail.com>
2024-06-09 16:24:28 +0700

    tedi: membuat LoginActivity

--- program/app/src/main/java/com/example/ta_pm_22a_rembo/LoginActivity.kt
+++ program/app/src/main/java/com/example/ta_pm_22a_rembo/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.example.ta_pm_22a_rembo
 
 import android.os.Bundle
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()

df1fc4b975401022216c7c6fd0426e6dc6f2fc0c
TediSuryaB <tedisuryabudi@gmail.com>
2024-06-18 20:07:13 +0700

    tedii : membuat codingan background.xml

---
+++ Program/app/src/main/java/com/uti/smart_farming/background.kt
@@ -0,0 +1,20 @@
+package com.uti.smart_farming
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class background : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_background)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

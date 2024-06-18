185c8afa615cee559fb84979dbe81cdd27d9f7a4
TediSuryaB <tedisuryabudi@gmail.com>
2024-06-18 19:42:15 +0700

    tedii : membuat linierlayout untk logout

--- Program/app/src/main/java/com/uti/smart_farming/tentangActivity.kt
+++ Program/app/src/main/java/com/uti/smart_farming/tentangActivity.kt
@@ -6,17 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 class tentangActivity : AppCompatActivity() {
-=======
-
-=======
-class tentangActivity : AppCompatActivity() {
->>>>>>> 3a58b6685a416be24707a83b458b84f5bfaab4c1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enablceEdgeToEdge()
+        enableEdgeToEdge()
         setContentView(R.layout.activity_tentang)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
